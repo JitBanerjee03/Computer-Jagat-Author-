86f33e77ba499777b4d076eb24b563dfc6bdcdc2
JitBanerjee03 <jitbanerjee294@gmail.com>
2025-07-04 16:09:05 +0530

    second commit

--- src/store/ContextProvider.jsx
+++ src/store/ContextProvider.jsx
@@ -41,7 +41,7 @@ export const ContextProvider = ({ children }) => {
 
         if (!token) {
             console.log('Author: No token found, redirecting to login');
-            window.location.href = 'https://journal-management-system-frontend.vercel.app/login';
+            window.location.href = 'https://journal-management-system-frontend.vercel.app';
             return;
         }
 
@@ -80,7 +80,7 @@ export const ContextProvider = ({ children }) => {
             } catch (error) {
                 console.error('Author: Initialization error:', error);
                 localStorage.removeItem('jwtToken');
-                window.location.href = 'https://journal-management-system-frontend.vercel.app/login';
+                window.location.href = 'https://journal-management-system-frontend.vercel.app';
             } finally {
                 setloader(false);
             }
@@ -97,7 +97,7 @@ export const ContextProvider = ({ children }) => {
         const storageListener = (e) => {
             if (e.key === 'logout-event') {
                 localStorage.removeItem('jwtToken');
-                window.location.href = 'https://journal-management-system-frontend.vercel.app/login';
+                window.location.href = 'https://journal-management-system-frontend.vercel.app';
             }
         };
         window.addEventListener('storage', storageListener);
